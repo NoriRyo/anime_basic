49a6eb5a3cc35ffd5ddc9f7110036146e576b1ba
Nori <fko2247018@stu.o-hara.ac.jp>
2022-09-30 12:19:30 +0900

    プレイヤーグラフィックの表示設定

--- SceneMain.cpp
+++ SceneMain.cpp
@@ -2,9 +2,29 @@
 #include <cassert>
 #include "SceneMain.h"
 
+namespace
+{
+	// グラフィックファイル名
+	const char* const kPlayarGraphicFilename = "data/char.png";
+
+	
+	// プレイヤーグラフィック分割数
+	constexpr int kPlayarGraphicDivX = 3;
+	constexpr int kPlayarGraphicDivY = 4;
+	constexpr int kPlayarGraphicDivNum = kPlayarGraphicDivX * kPlayarGraphicDivY;
+
+	// プレイヤーグラフィックサイズ
+	constexpr int kPlayarGraphicSizeX = 32;
+	constexpr int kPlayarGraphicSizeY = 32;
+
+}
+
 SceneMain::SceneMain()
 {
-	m_hPlayerGraphic = -1;
+	for (auto& handle : m_hPlayerGraphic)
+	{
+		handle = -1;
+	}
 }
 SceneMain::~SceneMain()
 {
@@ -14,15 +34,21 @@ SceneMain::~SceneMain()
 // 初期化
 void SceneMain::init()
 {
-	m_hPlayerGraphic = LoadGraph("data/char.png");
-	m_player.setHandle(m_hPlayerGraphic);
+	LoadDivGraph(kPlayarGraphicFilename, kPlayarGraphicDivNum,
+		kPlayarGraphicDivX, kPlayarGraphicDivY,
+		kPlayarGraphicSizeX, kPlayarGraphicSizeY, m_hPlayerGraphic);
+
+	m_player.setHandle(m_hPlayerGraphic[4]);
 	m_player.init();
 }
 
 // 終了処理
 void SceneMain::end()
 {
-	DeleteGraph(m_hPlayerGraphic);
+	for (auto& handle : m_hPlayerGraphic)
+	{
+		DeleteGraph(m_hPlayerGraphic[4]);
+	}
 }
 
 // 毎フレームの処理
